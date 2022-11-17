3ab237309236a78fa04b35e9f54d9758b4d787c1
Raznex <dudya15@yandex.ru>
2022-11-17 22:13:39 +0300

    hotfix

--- scripts/script.js
+++ scripts/script.js
@@ -27,46 +27,46 @@ function formSubmitHandler(evt) {
 popupElement.addEventListener('submit', formSubmitHandler);
 
 
-const cards = document.querySelectorAll(".element__like-button");
-const btn1 = cards[0];
-const btn2 = cards[1];
-const btn3 = cards[2];
-const btn4 = cards[3];
-const btn5 = cards[4];
-const btn6 = cards[5];
-
-const likeButton1Activated = function () {
-  btn1.classList.toggle("element__like-button_active");
-}
-
-btn1.addEventListener("click", likeButton1Activated);
-
-const likeButton2Activated = function () {
-  btn2.classList.toggle("element__like-button_active");
-}
-
-btn2.addEventListener("click", likeButton2Activated);
-
-const likeButton3Activated = function () {
-  btn3.classList.toggle("element__like-button_active");
-}
-
-btn3.addEventListener("click", likeButton3Activated);
-
-const likeButton4Activated = function () {
-  btn4.classList.toggle("element__like-button_active");
-}
-
-btn4.addEventListener("click", likeButton4Activated);
-
-const likeButton5Activated = function () {
-  btn5.classList.toggle("element__like-button_active");
-}
-
-btn5.addEventListener("click", likeButton5Activated);
-
-const likeButton6Activated = function () {
-  btn6.classList.toggle("element__like-button_active");
-}
-
-btn6.addEventListener("click", likeButton6Activated);
+// const cards = document.querySelectorAll(".element__like-button");
+// const btn1 = cards[0];
+// const btn2 = cards[1];
+// const btn3 = cards[2];
+// const btn4 = cards[3];
+// const btn5 = cards[4];
+// const btn6 = cards[5];
+//
+// const likeButton1Activated = function () {
+//   btn1.classList.toggle("element__like-button_active");
+// }
+//
+// btn1.addEventListener("click", likeButton1Activated);
+//
+// const likeButton2Activated = function () {
+//   btn2.classList.toggle("element__like-button_active");
+// }
+//
+// btn2.addEventListener("click", likeButton2Activated);
+//
+// const likeButton3Activated = function () {
+//   btn3.classList.toggle("element__like-button_active");
+// }
+//
+// btn3.addEventListener("click", likeButton3Activated);
+//
+// const likeButton4Activated = function () {
+//   btn4.classList.toggle("element__like-button_active");
+// }
+//
+// btn4.addEventListener("click", likeButton4Activated);
+//
+// const likeButton5Activated = function () {
+//   btn5.classList.toggle("element__like-button_active");
+// }
+//
+// btn5.addEventListener("click", likeButton5Activated);
+//
+// const likeButton6Activated = function () {
+//   btn6.classList.toggle("element__like-button_active");
+// }
+//
+// btn6.addEventListener("click", likeButton6Activated);
