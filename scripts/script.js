d9a0b5c8ac8c3434238de5820fc3dafb5033cb83
Raznex <dudya15@yandex.ru>
2022-11-17 18:30:37 +0300

    feat: popup working

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
-// Находим форму в DOM
 const popupElement = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
 const nameInput = popupElement.querySelector(".popup__name"); // Воспользуйтесь инструментом .querySelector()
 const jobInput = popupElement.querySelector(".popup__profession"); // Воспользуйтесь инструментом .querySelector()
 const popupCloseButton = popupElement.querySelector(".popup__close");
 const popupOpenButton = document.querySelector(".profile__edit-button");
+const formSubmit = popupElement.querySelector('.popup__save');
 
 const openPopupVisibility = function () {
   popupElement.classList.add("popup_is-opened");
@@ -15,38 +14,59 @@ const closePopupVisibility = function () {
 
 popupOpenButton.addEventListener("click", openPopupVisibility);
 popupCloseButton.addEventListener("click", closePopupVisibility);
-//
-//
-//
-//
-const elementButton = document.querySelector(".elements");
-const likeButton = elementButton.querySelector(".element__like-button");
 
-const likeButtonActivated = function () {
-  likeButton.classList.toggle("element__like-button_active");
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  const nameChange = document.querySelector('.profile__name');
+  const professionChange = document.querySelector('.profile__profession');
+  nameChange.textContent = nameInput.value;
+  professionChange.textContent = jobInput.value;
 }
 
- likeButton.addEventListener("click", likeButtonActivated);
+popupElement.addEventListener('submit', formSubmitHandler);
 
 
+const cards = document.querySelectorAll(".element__like-button");
+const btn1 = cards[0];
+const btn2 = cards[1];
+const btn3 = cards[2];
+const btn4 = cards[3];
+const btn5 = cards[4];
+const btn6 = cards[5];
 
+const likeButton1Activated = function () {
+  btn1.classList.toggle("element__like-button_active");
+}
 
-// // Обработчик «отправки» формы
-// function() {
+btn1.addEventListener("click", likeButton1Activated);
 
-// }
-// function formSubmitHandler (evt) {
-//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//                                                 // Так мы можем определить свою логику отправки.
-//                                                 // О том, как это делать, расскажем позже.
+const likeButton2Activated = function () {
+  btn2.classList.toggle("element__like-button_active");
+}
 
-//     // Получите значение полей jobInput и nameInput из свойства value
+btn2.addEventListener("click", likeButton2Activated);
 
-//     // Выберите элементы, куда должны быть вставлены значения полей
+const likeButton3Activated = function () {
+  btn3.classList.toggle("element__like-button_active");
+}
 
-//     // Вставьте новые значения с помощью textContent
-// }
+btn3.addEventListener("click", likeButton3Activated);
 
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler);
+const likeButton4Activated = function () {
+  btn4.classList.toggle("element__like-button_active");
+}
+
+btn4.addEventListener("click", likeButton4Activated);
+
+const likeButton5Activated = function () {
+  btn5.classList.toggle("element__like-button_active");
+}
+
+btn5.addEventListener("click", likeButton5Activated);
+
+const likeButton6Activated = function () {
+  btn6.classList.toggle("element__like-button_active");
+}
+
+btn6.addEventListener("click", likeButton6Activated);
